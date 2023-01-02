2a3880e134217f68c7e56f685b8c7faeadfe59b6
Marikamiki <118117702+Marikamiki@users.noreply.github.com>
2023-01-02 20:05:06 +0300

    add 3

---
+++ ex003/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+
+
+
+Console.Clear();
+
+
+Console.WriteLine("Enter amount of lines");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter amount of columns");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[linesVol, columnsVol];
+
+FillArrayRandomNumbers(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / linesVol;
+    Console.Write(avarage + "; ");
+}
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
